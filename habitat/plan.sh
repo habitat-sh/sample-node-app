4c3d4cba9da98ee3020c20e1794f021d9596e72e
Nell Shamrell <nellshamrell@gmail.com>
2017-09-29 14:43:47 -0700

    wip - adding in groundwork for dynamic version display in index page
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,4 +1,10 @@
 pkg_name=sample-node-app
 pkg_origin=core
 pkg_scaffolding="core/scaffolding-node"
+pkg_version="0.1.0"
+
+declare -A scaffolding_env
+
+# Define path to config file
+scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const nconf_file = process.env.APP_CONFIG || './default_config.json';
+
+var nconf = require('nconf');
+//nconf.file({ file: process.env.APP_CONFIG });
+
+nconf.file({ file: nconf_file });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to the Habitat Node.js demo app.' });
+  res.render('index', { title: nconf.get('message')});
 });
 
 module.exports = router;
